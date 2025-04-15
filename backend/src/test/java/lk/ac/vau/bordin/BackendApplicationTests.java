c1b8ac69fcef2b81a3f687845986dc6b89bba3f5
Thilankavishka <thilankavishka25@gmail.com>
2025-04-15 23:26:44 +0530

    Create Backend

---
+++ backend/src/test/java/lk/ac/vau/bordin/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.bordin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
